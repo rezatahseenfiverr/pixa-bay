9ceaf57d9b00c1f896346eb38a513a45a5e79f83
“[rezatahseenfiverr]” <[rezatahseenfiverr2gmail.com]>
2024-08-11 16:34:46 +0600

    M

--- src/components/PixaImage.jsx
+++ src/components/PixaImage.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import {Image} from "@nextui-org/react";
+import { NavLink } from "react-router-dom";
 
 export default function PixaImage({ImageInfo}) {
     
   return (
     
-    <Image
-    width={400}
-    height={300}
-    alt={ImageInfo.tags}
-    src={ImageInfo.webformatURL}
     
-  />
+  <NavLink target="_blank" to={ImageInfo.largeImageURL}><Image
+  width={400}
+  height={300}
+  alt={ImageInfo.tags}
+  src={ImageInfo.webformatURL}
   
+/></NavLink>
   );
 }
